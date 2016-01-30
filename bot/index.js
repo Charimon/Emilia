d2a0ba943745b8b7fccab85c73864cb781a4fd95
Charimon <acharkin@gmail.com>
2016-01-30 09:47:30 -0800

    made bot respond to individual chat

--- bot/index.js
+++ bot/index.js
@@ -9,28 +9,38 @@ var BotBrain = require("./botBrain.js");
 var user = conf.user;
 var chatEmitter = new events.EventEmitter();
 
-BotBrain.start(conf).then(function(bot){ return bot.listen(); }).then(function(blob){
-  var bot = blob.bot;
-  var event = blob.event;
-  switch(event.type) {
-    case "message":
-      debugger;
-      chatEmitter.emit('message', {event:event, bot:bot})
-      break;
-    case "event":
-      debugger;
-      chatEmitter.emit('event', {event:event, bot:bot})
-      break;
-  }
+BotBrain.start(conf).then(function(bot){
+  bot.api.listen(function(err, event) {
+    if(err) return console.error(err);
+    
+    switch(event.type) {
+      case "message":
+        chatEmitter.emit('message', {event:event, bot:bot})
+        break;
+      case "event":
+        chatEmitter.emit('event', {event:event, bot:bot})
+        break;
+    }
+  });
 });
 
 chatEmitter.on('message', function(blob){
   var event = blob.event;
   var bot = blob.bot;
-  if(bot.isAllowed(event)) {
+  
+  debugger
+  
+  if(!bot.isAllowed(event)) {
+    bot.whittyError(event);
+    return;
+  }
+  
+  debugger
+  if(bot.isIndividualConvo(event)) {
+    bot.individualRespond(event);
+  } else {
     bot.joinThread(event);
     bot.respond(event);
-  } else {
-    bot.whittyError(event);
   }
+  
 });
