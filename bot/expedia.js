c75ce498355a2266f402bfaa36a245df3081d162
Charimon <acharkin@gmail.com>
2016-01-30 06:17:32 -0800

    added missing var

--- bot/expedia.js
+++ bot/expedia.js
@@ -10,7 +10,7 @@ var expedia_key = conf.expedia_key;
 var idx = lunr(function () {
   this.field('name')
 })
-for (i = 0; i < airports.length; i++) { 
+for (var i = 0; i < airports.length; i++) { 
   var airport = airports[i]
   airport.id = i
   idx.add(airport)
