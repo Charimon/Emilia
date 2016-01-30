287eb65d35eaddc37ac1a8d6c23443daa55de502
Charimon <acharkin@gmail.com>
2016-01-30 00:06:55 -0800

    added fb

--- bot/index.js
+++ bot/index.js
@@ -2,6 +2,15 @@ var login = require("facebook-chat-api");
 var fs = require('fs');
 var conf = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
-var user = conf.user
+var user = conf.user;
 
-console.log(user.email)
+console.log(user.email);
+
+login({email: user.email, password: user.password}, function callback (err, api) {
+  if(err) return console.error(err);
+
+  var yourID = 0000000000000;
+  console.log("HERE");
+  //var msg = {body: "Hey!"};
+  //api.sendMessage(msg, yourID);
+});
