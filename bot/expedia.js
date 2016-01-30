0a7a758bf1dafda4a1b231939af97fc91767fa68
mcharkin <mcharkin@gmail.com>
2016-01-30 03:28:21 -0800

    cleaned up some stuff

--- bot/expedia.js
+++ bot/expedia.js
@@ -1,44 +1,6 @@
 var https = require('https');
 var fs = require('fs');
-// var airports = require('airports');
-var airports = [
-  {
-    iata: "SEA",
-    lon: "-122.301735",
-    iso: "US",
-    status: 1,
-    name: "Seattle Tacoma International Airport",
-    continent: "NA",
-    type: "airport",
-    lat: "47.44384",
-    size: "large"
-  },
-  {
-    iata: "YVR",
-    lon: "-123.17919",
-    iso: "CA",
-    status: 1,
-    name: "Vancouver International Airport",
-    continent: "NA",
-    type: "airport",
-    lat: "49.1947",
-    size: "large"
-  },
-  {
-    iata: "SFO",
-    lon: "-122.38988",
-    iso: "US",
-    status: 1,
-    name: "San Francisco International Airport",
-    continent: "NA",
-    type: "airport",
-    lat: "37.615215",
-    size: "large"
-  }
-]
-
-
-var options = {} //put the startup options you want here
+var airports = require('airports');
 var lunr = require('lunr');
 
 var conf = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
@@ -51,8 +13,6 @@ var idx = lunr(function () {
 for (i = 0; i < airports.length; i++) { 
   var airport = airports[i]
   airport.id = i
-  console.log("inserting")
-  console.log(airport)
   idx.add(airport)
 }
 
