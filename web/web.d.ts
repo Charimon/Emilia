cd0b34745f777b97b4f332d37875db57f772dfeb
Charimon <acharkin@gmail.com>
2016-01-30 16:35:49 -0800

    stuff

--- web/web.d.ts
+++ web/web.d.ts
@@ -15,3 +15,13 @@ declare module "react-retina-image" {
   var RetinaImage: any 
   export = RetinaImage;  
 }
+
+declare module "react-cookie" {
+  var cookie: any 
+  export = cookie;
+}
+
+declare module "parse-react" {
+  var parse: any
+  export = parse;
+}
