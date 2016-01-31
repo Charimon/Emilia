64a78bee6ee28e127b6bc2449573837a4d1448fd
mcharkin <mcharkin@gmail.com>
2016-01-31 03:10:39 -0800

    added missing files

---
+++ bot/weekend.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var DateFormat = require('dateformat');
+var DateMath = require('date-math');
+
+class Weekend {
+  
+  constructor(weeksFromNow) {
+    var now = new Date()
+    var sunday = DateMath.week.ceil(now);
+    
+    var sundayWeekend = DateMath.week.shift(sunday, weeksFromNow)
+    var fridayWeekend = DateMath.day.shift(sundayWeekend,-2)
+    
+    this.start = fridayWeekend
+    this.end = sundayWeekend
+  
+  }
+  
+  get pretty() {
+    // return `${DateFormat(this.start, "isoDateTime")} - ${DateFormat(this.end, "isoDateTime")}`
+    
+    return `${DateFormat(this.start, "mmm d")} - ${DateFormat(this.end, "mmm d")}`
+  }
+  
+  static nextGoodWeeknds() {
+    
+    
+    var res =  [new Weekend(3), new Weekend(4), new Weekend(5)]
+    return res;
+  }
+  
+}
+
+// var testW = new Weekend(5)
+
+// console.log( Weekend.nextGoodWeeknds().map ( (w) => w.pretty ) )
+
+module.exports = Weekend;
